39a9acd83c55277462ae11120b02385a3872f6b2
CL1-PC07 <CL1-PC07@DESKTOP-0KH4I5L>
2025-07-18 09:16:11 +0800

    Sample program ACT2B

---
+++ src/main/java/com/mycompany/vincent2b/Vincent2B.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vincent2b;
+
+/**
+ *
+ * @author CL1-PC07
+ */
+public class Vincent2B {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
